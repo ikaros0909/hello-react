3c2e93318f7a44f10f08e1e7f8488edb4dd4a740
unknown <danny@jinhak.com>
2018-05-27 22:20:00 +0900

    aa

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './Contact';
+
+class App extends React.Component{
+    render(){
+        return(
+            <Contact/>
+        )
+    }
+}
+
+/*다른 코드에서 이 클래스를 불러서 사용할수 있도록 */
+export default App;
